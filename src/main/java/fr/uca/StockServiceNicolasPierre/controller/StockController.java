65c553c02b59c718193b44cbb9dc389ebf0e2d85
gabriellepierre <gabrielle.pierre19@hotmail.fr>
2023-05-22 01:04:52 +0200

    :zap: update corr try v1

--- src/main/java/fr/uca/StockServiceNicolasPierre/controller/StockController.java
+++ src/main/java/fr/uca/StockServiceNicolasPierre/controller/StockController.java
@@ -38,8 +38,8 @@ public class StockController {
             @RequestParam String from,
             @RequestParam String to) {
         try {
-            return bookService.getBookStock(isbn, updateCorr(corr, this.SHOPPING_ID, this.SERVICE_ID), this.SHOPPING_ID,
-                    this.SERVICE_ID);
+            return bookService.getBookStock(isbn, updateCorr(corr, this.SERVICE_ID, this.SHOPPING_ID), this.SERVICE_ID,
+                    this.SHOPPING_ID);
         } catch (BookNotFoundException e) {
             throw new BookNotFoundException("Book not found");
         } catch (InternalErrorException e) {
